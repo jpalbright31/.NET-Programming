45e0c60ebab75b28cae75b7facafa509e86abcdc
Vladimir Stratiev <vladimir_stratiev@yahoo.com>
2016-10-19 15:22:01 +0300

    Square Root.cs

---
+++ CSharp/CSharpFundamentals/Introduction-to-Programming/SquareRoot.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Homework1
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            double x = 12345;
+            Console.WriteLine(Math.Sqrt(x));
+        }
+    }
+}
